7a53c7f905e6b6bda9bbf53ee80b4ce2f2104139
FadilRifqi <fadilataullahrifqi@gmail.com>
2025-11-18 12:22:18 +0700

    menambahkan tugas tugas sebelumnya

---
+++ environment-setup/tes.py
@@ -0,0 +1,21 @@
+import numpy
+import pandas
+import moviepy
+import cv2
+import matplotlib
+import skimage
+import librosa
+import soundfile
+import scipy
+import PIL
+
+print("NumPy version:", numpy.__version__)
+print("Pandas version:", pandas.__version__)
+print("MoviePy version:", moviepy.__version__)
+print("OpenCV version:", cv2.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("scikit-image version:", skimage.__version__)
+print("Librosa version:", librosa.__version__)
+print("SoundFile version:", soundfile.__version__)
+print("SciPy version:", scipy.__version__)
+print("Pillow version:", PIL.__version__)
